b10d9e440cd1f8c0807bd4279a054a358e7fa1e8
yonny <yonnymesa96@gmail.com>
2025-11-19 21:15:39 -0500

    UPDATE models and databese

--- models/expense.py
+++ models/expense.py
@@ -1,6 +1,12 @@
-class expense():
-    def __init__(self,date,bill,quantity):
-        self.date = date 
-        self.bill = bill
-        self.quantity = quantity
+from config.database import *
+class expense_model():
+    def insert_expense(self,idProduct,date,bill,amount,idUser):
+        run_query ("INSERT INTO expense (idProduct,date,bill,amount,idUser) VALUES(%s,%s,%s,%s)",(idProduct,date,bill,amount,idUser),False)
+        return True
+    def delete_expense(self,idExpense):
+        run_query("DELETE FROM expense WHERE idExpense = %s",(idExpense,),False)
+        return True
+    def update_expense(self,idExpense,idProduct,date,bill,amount,idUser):
+        run_query ("UPDATE expense SET idProduct = %s,date = %s,bill = %s,amount = %s,idUser =%s WHERE idExpense = %s ",(idProduct,date,bill,amount,idUser),False)
+        return True
         

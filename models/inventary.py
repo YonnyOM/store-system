1c8a46ce37c5028bacdf693fca4cb2a0de11c31b
yonny <yonnymesa96@gmail.com>
2025-11-20 17:24:31 -0500

    Finish models and Delete files MER

--- models/inventary.py
+++
@@ -1,5 +0,0 @@
-class inventory():
-    def __init__(self,purchase,sale,amount):
-        self.purchase = purchase
-        self.salse = sale
-        self.amount = amount
--- models/sale.py
+++ models/sale.py
@@ -1,4 +1,18 @@
-class sale():
-    def __init__(self,date,amount):
-        self.date = date
-        self.amount = amount
+from config.database import *
+
+class sale_model():
+    def insert_sale(self,idProduct,date,amount,idUser):
+        run_query("INSERT INTO sales (idProduct,date,amount,idUser) VALUES(%s,%s,%s,%s)",(idProduct,date,amount,idUser),False)
+        return True
+    def delete_sale(self,idSale):
+        run_query("DELETE FROM sales WHERE idSale = %s",(idSale,),False)
+        return True
+    def update_sale(self,idProduct,date,amount,idUser,idSale):
+        date = run_query("UPDATE sales SET idProduct = %s,date = %s,amount = %s,idUser = %s WHERE idSale = %s",(idProduct,date,amount,idUser,idSale),False)
+        return True
+    def fetch_sale(self,idSale):
+        data = run_query("SELECT *FROM sales WHERE idSale = %s",(idSale,),True)
+        return data
+    def fetch_sale_all(self,idSale):
+        data = run_query("SELECT *FROM sales",None,True)
+        return data
